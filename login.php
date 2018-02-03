0958e8c4809ec38d6e39ca4e5ee3c54f33aab3f3
dheanatasilvia <dheasilvia74@gmail.com>
2018-02-03 14:29:55 -0800

    mengupload login.php

---
+++ login.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login</title>
+</head>
+<body>
+	<form action="tabel.php">
+		<center>
+		LOGIN FORM
+		<hr>
+			<table cellpadding="5">
+				<tr>
+					<td>Username</td>
+					<td>:</td>
+					<td><input type="" name=""></td>
+				</tr>
+				<tr>
+					<td>Password</td>
+					<td>:</td>
+					<td><input type="" name=""></td>
+				</tr>	
+				<tr>
+					<td colspan="2"> <button>Login</button></td>
+				</tr>	
+			</table>
+		</center>
+	</form>
+
+</body>
+</html>
